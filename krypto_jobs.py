5e2426dc3886aa3dae54816ef0b3948ebfe9dfaf
aletorrescande <aletcrb@gmail.com>
2023-10-30 15:20:34 -0700

    added additional files

--- krypto_jobs.py
+++ krypto_jobs.py
@@ -90,28 +90,28 @@ from crypto_wallet import generate_account, get_balance, send_transaction
 candidate_database = {
     "Lane": [
         "Lane",
-        "0xaC8eB8B2ed5C4a0fC41a84Ee4950F417f67029F0",
+        "0x06E218151507E43894C0Cd907d25051839e4E22e",
         "4.3",
         0.20,
         "Images/lane.jpeg",
     ],
     "Ash": [
         "Ash",
-        "0x2422858F9C4480c2724A309D58Ffd7Ac8bF65396",
+        "0x047bAAB5A78bA4fB2C278c9EFE17AA1C85Ca0ced",
         "5.0",
         0.33,
         "Images/ash.jpeg",
     ],
     "Jo": [
         "Jo",
-        "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45",
+        "0xC62Debb5500bA277DAf9C7CabaCed4F728070498",
         "4.7",
         0.19,
         "Images/jo.jpeg",
     ],
     "Kendall": [
         "Kendall",
-        "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45",
+        "0xa13F8Fe7673D22dc9d953D98c8fc7Bd153738491",
         "4.1",
         0.16,
         "Images/kendall.jpeg",
